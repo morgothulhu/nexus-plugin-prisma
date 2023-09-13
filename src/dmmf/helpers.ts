eba9070421fb82c73034ed11e131da5c745a3722
Morgothulhu <francois@rleyh.com>
2023-09-13 07:24:35 -0700

    fixing build

--- src/dmmf/helpers.ts
+++ src/dmmf/helpers.ts
@@ -3,7 +3,7 @@
  */
 import { DMMF } from '@prisma/client/runtime/library'
 
-export function getTypeName(type: DMMF.ArgType | DMMF.InputType | DMMF.OutputType): string {
+export function getTypeName(type: string | DMMF.SchemaEnum | DMMF.InputType | DMMF.OutputType): string {
   if (typeof type === 'string') {
     return type
   }
